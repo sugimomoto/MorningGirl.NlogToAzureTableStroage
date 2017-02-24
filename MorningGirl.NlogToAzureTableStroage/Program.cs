0f526c888d0de87a0974564cd54154f3d0c096a3
sugimomoto@gmail.com <sugimomoto@gmail.com>
2017-02-24 23:28:34 +0900

    新規作成

--- MorningGirl.NlogToAzureTableStroage/Program.cs
+++ MorningGirl.NlogToAzureTableStroage/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,15 @@ namespace MorningGirl.NlogToAzureTableStroage
 {
     class Program
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Start Log Test");
+
+            logger.Error("Errorですよ！！");
+
+            Console.WriteLine("End Log Test");
         }
     }
 }
